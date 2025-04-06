f97b31115026429d02abfbf2e63d6de53f6a10f7
a5-stable <sh07e1916@gmail.com>
2025-04-06 22:43:24 +0900

    add json BNF

--- parser.rb
+++ parser.rb
@@ -3,3 +3,7 @@ class Parser
 end
 
 # JSON BNF
+# <jsonExpr>  ::= "{" <string> ":" ( <jsonValue> | <jsonExpr> ) "}"
+# <jsonValue> ::= <array> | <primary>
+# <array>     ::= "[" <primary> ("," <primary>)* "]"
+# <primary>   ::= STRING | NUMBER
